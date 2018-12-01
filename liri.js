6769b728f3d7bac6a79bace7c2cec07638a30c04
Carlos M Garcia <cgarcia101015@gmail.com>
2018-12-01 14:24:04 -0500

    working on spotify api

--- liri.js
+++ liri.js
@@ -3,18 +3,10 @@
 require("dotenv").config();
 
 // // spotify variables
-var importKeys = require("./keys.js");
-var SpotifyWebApi = require('spotify-web-api-node');
-var spotify = new Spotify(keys.spotify);
+// var spotify = require("./keys.js");
+var Spotify = require('node-spotify-api');
+// spotify = new Spotify(keys.spotify);
 
-var SpotifyWebApi = require('spotify-web-api-node');
-
-// credentials are optional
-var spotifyApi = new SpotifyWebApi({
-clientId: '08ed08fb5e044421813e9c183a7d65a0',
-clientSecret: 'cc7c7e361b4848fb9312c5947847ca4c',
-redirectUri: 'http://www.example.com/callback'
-});
 
 // BandsInTown Variables
 var axios = require("axios");
@@ -40,24 +32,16 @@ if (command === 'concert-this') {
 // functions
 
 function spotify() {
-    // Get Elvis' albums
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-    function(data) {
-      console.log('Artist albums', data.body);
-    },
-    function(err) {
-      console.error(err);
-    }
-  );
-    
-    
-    // $.ajax({
-    //     method: "GET",
-    //     url: `https://api.spotify.com/v1/search?q=track:antarctica&type=track`
-    //   }).done(function(data) {
-    //     console.log(data);
-    //   });
+    var spotify = new Spotify(keys.spotify);
+    spotify.search({ type: 'track', query: 'All the Small Things', limit: 1 }, function(err, data) {
+        if (err) {
+            return console.log('Error occured: ' + err);
+        }
+    console.log(data);
+    });
 };
+
+   
 
 function BandsInTown() {
     var artist = parameter;
